120181b07f09c6c8a0b68d85c21b31ab95f3d9c5
AishaD <AishaDahir75@gmail.com>
2022-12-01 16:49:04 +0000

    DI-84 make VCF smoll

--- src/eggd_CANRISK_prep.sh
+++ src/eggd_CANRISK_prep.sh
@@ -10,7 +10,7 @@ main() {
     # Install packages from the python asset
     pip3 install /pytz-*.whl /numpy-*.whl /pandas-*.whl
 
-    
+    bedtools intersect -a $vcf_path -b $PRS_variants_path -header > NA12878-NA12878-1-CEN-F-EGG5_markdup_recalibrated_Haplotyper_filteredPOS.vcf
 
     filtered_VCF=$(dx upload filtered_VCF --brief)
     dx-jobutil-add-output filtered_VCF "$filtered_VCF" --class=file
