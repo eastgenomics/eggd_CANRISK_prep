0b5c52244599eb9caf73691d6e4876f92a2bf03f
AishaD <AishaDahir75@gmail.com>
2022-12-05 11:55:17 +0000

    DI-84 combine vcf & prs filename as output filename

--- src/eggd_CANRISK_prep.sh
+++ src/eggd_CANRISK_prep.sh
@@ -10,8 +10,20 @@ main() {
     # Install packages from the python asset
     pip3 install /pytz-*.whl /numpy-*.whl /pandas-*.whl
 
-    bedtools intersect -a $vcf_path -b $PRS_variants_path -header > NA12878-NA12878-1-CEN-F-EGG5_markdup_recalibrated_Haplotyper_filteredPOS.vcf
+    # make the bed file chr, start and end, then remove the first rows
+    # which is the alpha value for the PRS risk and headers
+    awk -F "," '{print $1"\t"$2-1"\t"$2}' $PRS_variants_path | tail -n +3  > PRS_variants.bed
 
-    filtered_VCF=$(dx upload filtered_VCF --brief)
-    dx-jobutil-add-output filtered_VCF "$filtered_VCF" --class=file
+    # intersect the VCF and PRS variant list to save mem in python
+    vcf_filename=${vcf_path##*/}
+    PRS_variants_filename=${PRS_variants_path##*/}
+
+    out_filename=${vcf_filename%%.*}_${PRS_variants_filename%.*}
+    echo $out_filename
+    bedtools intersect -a $vcf_path -b  PRS_variants.bed -header > ${out_filename}.vcf
+
+    head ${out_filename}.vcf
+
+    # filtered_VCF=$(dx upload filtered_VCF --brief)
+    # dx-jobutil-add-output filtered_VCF "$filtered_VCF" --class=file
 }
