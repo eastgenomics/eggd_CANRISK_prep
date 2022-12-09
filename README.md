ac665d224731ecbc18e1744990a8a0a9c1775cd1
AishaD <AishaDahir75@gmail.com>
2022-12-09 10:55:24 +0000

    DI-84 update to work with gVCF and update readme

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # eggd_CANRISK_prep
-This app filters and formats VCFs to calculate PRS scores using the CANRISK tool
+
+## What does this app do?
+
+This app filters and formats VCFs to calculate PRS scores using the online CanRisk tool (https://www.canrisk.org/canrisk_tool/).
+
+## What inputs are required for this app to run?
+
+- gVCF (VCF also works but this is not accurate as homozygous reference variants are missed)
+- PRS file from CanRisk (https://canrisk.atlassian.net/wiki/spaces/FAQS/pages/35979266/What+variants+are+used+in+the+PRS)
+- Reference genome
+
+## How does this app work?
+
+The app filters the gVCF to the PRS variants. Then it normalises multiallelic variants. This intermediate file is run through the custom python script to reformat the VCF and add any missing variants with null genotype "./."
+
+
+## What does this app output?
+
+A single VCF with the PRS name appended to the VCF name.
+
+## What limitations does this app have?
+
+This has been tested for germline gVCFs outputted from Sentieon DNAnexus app.
+
+## This app was created by East Genomics GLH
--- resources/home/dnanexus/vcf_filtering.py
+++ resources/home/dnanexus/vcf_filtering.py
@@ -45,6 +45,8 @@ def read_vcf_in(args, SAMPLE):
     vcf_df['QUAL'] = "."
     vcf_df['INFO'] = "."
 
+    # g.vcf has noref info in it, the first is usually the alt variant
+    vcf_df['ALT'] = vcf_df['ALT'].str.split(',', expand=True)[0]
 
     return vcf_df
 
@@ -56,36 +58,14 @@ def main():
 
     # Read in VCF
     vcf_df = read_vcf_in(args, SAMPLE)
-    # Read in the PRS varaints file
     PRS = pd.read_csv(args.PRS_variant_file, sep=",", skiprows=1,
                         compression='infer')
 
-    # Now loop through our VCF, get the chr & pos of each variant and check
-    # that the REF>ALT matches whats in PRS variants file. If there is
-    # a match keep, else delete row
-    variants_to_drop = []
-    for variant in range(0, len(vcf_df.index)):
-        chr = vcf_df.loc[variant,"CHROM"]
-        pos = vcf_df.loc[variant,"POS"]
-        ref = vcf_df.loc[variant,"REF"]
-        alt = vcf_df.loc[variant,"ALT"]
-        PRS_subset = PRS[(PRS['Chromosome'] == chr) & (PRS['Position'] == pos)]
-        if PRS_subset.empty:
-            next
-        else:
-            if PRS_subset['Reference_Allele'].values[0] == ref and PRS_subset['Effect_Allele'].values[0] == alt:
-                next
-            else:
-                print("Incorrect Ref>Alt change, removing this variant")
-                variants_to_drop.append(variant)
-
-    # remove variants that have unexpected Ref>Alt change
-    vcf_df = vcf_df.drop(variants_to_drop)
     # CANRISK is sassy and needs all sites. To deal with it, we will
     # check if position and REF>ALT dont match, then we can add this is
     # but genotype 0/0
-
-    print(vcf_df)
+    match = 0
+    no_match = 0
 
     new_vcf_df = pd.DataFrame()
 
@@ -94,9 +74,12 @@ def main():
         pos = PRS.loc[variant,"Position"]
         ref = PRS.loc[variant,"Reference_Allele"]
         alt = PRS.loc[variant,"Effect_Allele"]
-        vcf_df2_subset = vcf_df[(vcf_df['CHROM'] == chr) & (vcf_df['POS'] == pos) & (vcf_df['REF'] == ref) & (vcf_df['ALT'] == alt)]
-        # if the chr,pos,ref and alt doesnt exist in our VCF, we should include it,
-        if vcf_df2_subset.empty:
+        vcf_df_subset = vcf_df[(vcf_df['CHROM'] == chr) & (vcf_df['POS'] == pos) & (vcf_df['REF'] == ref) & (vcf_df['ALT'] == alt)]
+        # if the chr,pos,ref and alt from PRS doesnt exist in our VCF,
+        # we need to append this to a new df with details from PRS with
+        # GT = "./". If there is data, then append this to the empty df
+        if vcf_df_subset.empty:
+            no_match = no_match +1
             new_row = pd.Series({'CHROM': chr,
                     'POS': pos,
                     'ID': '.',
@@ -106,23 +89,27 @@ def main():
                     'FILTER': '.',
                     'INFO': '.',
                     'FORMAT': 'GT',
-                    SAMPLE:'0/0'
+                    SAMPLE:'./.'
                     })
             #append row to the dataframe
             # if you concate a dict it makes it into a single col so transpose
             new_vcf_df = pd.concat([new_vcf_df, new_row.to_frame().T], ignore_index=True)
+        else:
+            #if there is a match then add this to the new_df
+            match = match + 1
+            new_vcf_df = pd.concat([new_vcf_df, vcf_df_subset], ignore_index=True)
 
+    print(f"Number of variants in VCF as expected : {match}")
+    print(f"Number of variants in VCF modified as it was not expected genotype : {no_match}")
 
-    # this contains all the GT's that don't exist/match what CANRISK wants
-    # let's combine vcf_df and new_vcf_df
-    final_vcf =  pd.concat([vcf_df, new_vcf_df], ignore_index=True)
-    final_vcf['CHROM'] = 'chr' + final_vcf['CHROM'].astype(str)
-    final_vcf = final_vcf.rename(columns={"CHROM": "#CHROM"})
-    print(final_vcf)
-    final_vcf.to_csv(
+    new_vcf_df = new_vcf_df.rename(columns={"CHROM": "#CHROM"})
+    new_vcf_df = new_vcf_df[["#CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO", "FORMAT", SAMPLE]]
+    print(new_vcf_df)
+    new_vcf_df.to_csv(
             args.vcf.split(".")[0] + "_CANRISK.vcf",
             sep="\t", index=False, header=True
             )
+
 
 if __name__ == "__main__":
 
